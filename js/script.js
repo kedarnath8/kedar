5428522d6043c67b82f1b19a3ddd1ace77a51f70
kedarnath8 <43844309+kedarnath8@users.noreply.github.com>
2018-10-31 10:13:26 +0530

    initial commit

--- js/script.js
+++ js/script.js
@@ -17,3 +17,17 @@ $( ".hamburger" ).show();
 });
 
 });
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('#scroll').fadeIn();
+        } else {
+            $('#scroll').fadeOut();
+        }
+    });
+    $('#scroll').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+});
